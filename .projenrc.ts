8dd5b932447972c8c0257809efebe0485ea71310
Liam-Johnston <liam.johnston@v2.digital>
2023-10-22 19:08:23 +1100

    chore(ignore-todo): added todo to gitignore

--- .projenrc.ts
+++ .projenrc.ts
@@ -6,15 +6,10 @@ const project = new BunTypescript({
   repository: 'https://github.com/Liam-Johnston/projen-bun-ts',
   name: 'bun-ts-projen',
   tsconfigFilename: 'tsconfig.dev.json',
-  keywords: [
-    'bun',
-    'projen',
-    'jsii',
-    'project-template'
-  ]
+  keywords: ['bun', 'projen', 'jsii', 'project-template'],
 });
 
-project.gitignore.addPatterns('tsconfig.json', '.jsii', 'lib/');
+project.gitignore.addPatterns('tsconfig.json', '.jsii', 'lib/', 'todo');
 
 project.package.addDevDeps(
   'jsii',
@@ -37,17 +32,17 @@ project.package.addField('jsii', {
 
 project.package.setScript('build', 'jsii --silence-warnings=reserved-word');
 
-project.package.addField('files', ['lib', '.jsii'])
+project.package.addField('files', ['lib', '.jsii']);
 
-project.package.addVersion('0.0.1')
+project.package.addVersion('0.0.1');
 
-const ignoreFile = new IgnoreFile(project, '.npmignore')
+const ignoreFile = new IgnoreFile(project, '.npmignore');
 
-ignoreFile.addPatterns('node_modules/')
+ignoreFile.addPatterns('node_modules/');
 
 new License(project, {
   spdx: project.package.license || 'Apache-2.0.txt',
   copyrightOwner: 'Liam Johnston',
-})
+});
 
 project.synth();
